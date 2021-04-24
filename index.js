2c81c220a5e04dba67ef81f19e283ebf728fc0a6
Patrick Dunn <pmdunn78@gmail.com>
2021-04-24 18:17:19 -0500

    init & writeREADME functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('welcome to README generator')
+const inquirer = require('inquirer');
+
+
+
+const init = () => {
+    console.log('welcome to README generator');
+
+    const writeREADME = arr => {
+        false.writeFile('./dist/README.md', arr, function(err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Please find generated file in dist folder.')
+        })
+    }
+}
+
+init();
+
