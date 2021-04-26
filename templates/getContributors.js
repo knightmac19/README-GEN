f6d6db7b6aafb884b7662706fa4342f1ee0cbac3
Patrick Dunn <pmdunn78@gmail.com>
2021-04-26 15:17:17 -0500

    correct spacing for contributors section

--- templates/getContributors.js
+++ templates/getContributors.js
@@ -1,10 +1,11 @@
-const getContributors = arr => {
+const getContributors = (arr) => {
     let resultArr = [];
     for (var i = 0; i < arr.length; i++) {
-        let string = `- ${arr[i].name} // [${arr[i].email}](mailto:${arr[i].email}) // [github.com/${arr[i].github}](https://github.com/${arr[i].github})`
+        let string = `- ${arr[i].name} // [${arr[i].email}](mailto:${arr[i].email}) // [github.com/${arr[i].github}](https://github.com/${arr[i].github})  `;
         resultArr.push(string);
     }
-    return resultArr.join('<br>');
+
+    return resultArr.join('\n');
 
 };
 
